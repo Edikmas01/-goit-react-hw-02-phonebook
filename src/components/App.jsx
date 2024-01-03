e7445f67ecd9e6e0f61e8b64dccb9f500ab06edd
Edikmas01 <edikmaslovskiy@gmail.com>
2024-01-03 11:37:23 +0100

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { nanoid } from 'nanoid';
+import { Phonebook } from "./Phonebook/Phonebook";
+import { Contacts } from "./Contacts/Contacts";
+import { Filter } from "./Filter/Filter";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+
+  handleSubmit = (name, number) => {
+    const { contacts } = this.state;
+    const isNameAlreadyExists = contacts.some(contact => contact.name === name);
+    if (isNameAlreadyExists) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  handleChange = value => {
+    console.log(value);
+    this.setState({ filter: value });
+  };
+
+  handleDeleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    const visibleContacts = contacts.filter(
+      ({ name, number }) =>
+        name.toLowerCase().includes(normalizedFilter) ||
+        number.includes(normalizedFilter)
+    );
+
+    return (
+      <div>
+        <h1 className="title">Phonebook</h1>
+
+        <Phonebook onSubmit={this.handleSubmit} />
+        <h1 className="title">Contacts</h1>
+        <Filter onChange={this.handleChange} filter={filter} />
+        <Contacts
+          contacts={visibleContacts}
+          onChange={this.handleChange}
+          filter={filter}
+          onDelete={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
 };
+
+export default App;
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+
+
+export class Contacts extends Component {
+    render() {
+        const { contacts, onDelete } = this.props;
+       
+    return (
+      <>
+        <ul>
+          {contacts.map(({ name, number, id }) => (
+            <li key={id}>
+              {' '}
+              {name}: {number}
+              <button
+                style={{
+                  color: 'red',
+                  margin: 5,
+                }}
+                className="contact-btn"
+                onClick={() => onDelete(id)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
